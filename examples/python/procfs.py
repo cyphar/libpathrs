cyphar/libpathrs

commit b2f24a201b8e1de86a9051fbdc17ad4a29578cde

examples: relicense to MPL-2.0
I am the sole author of this code, and so there is no need for approval
from any other contributors.

For posterity, at the time of this relicense there were only two commits
by other authors to the examples/ source code, and all of the code has
either been completely removed since or is too minor to be
copyrightable:

commit da0b28624b95 ("examples: fix typo") by Jakub Wilk:

  This one-line patch only fixes a typographical error in a comment, and
  is thus a minor patch that is not copyrightable (as per the GNU
  guidelines).

commit e6737f8369bf ("examples: create a golang 'cat' binding example")
by Maxim Zhiburt:

  This patch implemented an example Go program, and a program of the
  same name still exists in exmaples/go/cat.go. However, as part of
  commit b316bb7ecacf ("go bindings: completely rewrite Go bindings to
  new API") the bindings were completely rewritten and thus the Go
  program using the bindings also needed to be completely rewritten.

  As such, the original program written by Maxim Zhiburt is no longer
  present and thus their copyrights are not at issue. (For posterity, I
  have included their historical authorship information, but this is not
  a statement on their copyrights.)

Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

examples/python/procfs.py
@@ -1,0 +1,167 @@
+#!/usr/bin/env python3
+# libpathrs: safe path resolution on Linux
+# Copyright (C) 2019-2024 Aleksa Sarai <cyphar@cyphar.com>
+# Copyright (C) 2019-2024 SUSE LLC
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+# File: examples/python/procfs.py
+#
+# An example program which does some basic procfs operations.
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(__file__) + "/../contrib/bindings/python")
+import pathrs
+
+
+def chomp(s):
+    for nl in ["\r\n", "\r", "\n"]:
+        if s.endswith(nl):
+            return s[: -len(nl)]
+    return s
+
+
+# TODO: Pretty-print errors...
+
+
+def base_str(args):
+    base = args.procfs_base
+    if base == pathrs.PROC_ROOT:
+        return "/proc"
+    elif base == pathrs.PROC_SELF:
+        return "/proc/self"
+    elif base == pathrs.PROC_THREAD_SELF:
+        return "/proc/thread-self"
+    elif args._pid is not None:
+        return f"/proc/{args._pid}"
+    return "<unknown proc base>"
+
+
+def do_open(args):
+    base = args.procfs_base
+    subpath = args.subpath
+
+    # TODO: Support O_* flags.
+    extra_flags = 0
+    if not args.follow_trailing:
+        extra_flags |= os.O_NOFOLLOW
+
+    if args.debug:
+        print(f"fullpath: {base_str(args)}/{subpath}")
+        print("---")
+
+    with pathrs.proc_open(base, subpath, extra_flags=extra_flags) as f:
+        for line in f:
+            print(chomp(line))
+
+
+def do_readlink(args):
+    base = args.procfs_base
+    subpath = args.subpath
+
+    if args.debug:
+        print(f"fullpath: {base_str(args)}/{subpath}")
+        print("---")
+
+    link_target = pathrs.proc_readlink(base, subpath)
+    print(link_target)
+
+
+def main(*args):
+    import argparse
+
+    class ProcPidAction(argparse.Action):
+        def __init__(self, option_strings, dest, nargs=None, **kwargs):
+            if nargs is not None:
+                raise ValueError("nargs not allowed")
+            super().__init__(option_strings, dest, **kwargs)
+
+        def __call__(self, parser, namespace, values, option_string=None):
+            pid = int(values)
+            setattr(namespace, "_pid", pid)
+            setattr(namespace, self.dest, pathrs.PROC_PID(pid))
+
+    parser = argparse.ArgumentParser(prog="procfs.py")
+    parser.add_argument("--debug", action="store_true", help="output debug messages")
+
+    procfs_base = parser.add_argument_group(
+        title="base path",
+        description="Specify what base directory to use as the root of the procfs operation.",
+    ).add_mutually_exclusive_group(required=True)
+
+    procfs_base.add_argument(
+        "-R",
+        "--root",
+        dest="procfs_base",
+        action="store_const",
+        const=pathrs.PROC_ROOT,
+        help="/proc",
+    )
+    procfs_base.add_argument(
+        "-s",
+        "--self",
+        dest="procfs_base",
+        action="store_const",
+        const=pathrs.PROC_SELF,
+        help="/proc/self",
+    )
+    procfs_base.add_argument(
+        "--thread-self",
+        dest="procfs_base",
+        action="store_const",
+        const=pathrs.PROC_THREAD_SELF,
+        help="/proc/thread-self",
+    )
+    procfs_base.add_argument(
+        "-p",
+        "--pid",
+        metavar="PID",
+        dest="procfs_base",
+        action=ProcPidAction,
+        help="/proc/$PID (racey)",
+    )
+    procfs_base.set_defaults(_pid=None)
+
+    subparser = parser.add_subparsers(help="procfs operations")
+
+    # procfs.py readlink <subpath>
+    readlink_cmd = subparser.add_parser("readlink", help="safe readlink")
+    readlink_cmd.set_defaults(func=do_readlink)
+
+    # procfs.py open <subpath>
+    open_cmd = subparser.add_parser("open", help="open(O_NOFOLLOW)")
+    open_cmd.add_argument(
+        "--follow-trailing",
+        dest="follow_trailing",
+        action=argparse.BooleanOptionalAction,
+        default=True,
+        help="whether to follow trailing symlinks when opening paths (this allows you to re-open magic-links)",
+    )
+    # TODO: Support O_* flags.
+    open_cmd.set_defaults(func=do_open)
+
+    parser.add_argument("subpath")
+
+    args = parser.parse_args(args)
+    if args.debug:
+        print(f"procfs base: {args.procfs_base:#x} (pid={args._pid})")
+        print(f"subcommand: {args.func}")
+        print(f"subpath: {args.subpath}")
+    args.func(args)
+
+
+if __name__ == "__main__":
+    main(*sys.argv[1:])

examples/Makefile
@@ -1,20 +1,13 @@
-# SPDX-License-Identifier: LGPL-3.0-or-later
+# SPDX-License-Identifier: MPL-2.0
 #
 # libpathrs: safe path resolution on Linux
 # Copyright (C) 2019-2025 Aleksa Sarai <cyphar@cyphar.com>
 # Copyright (C) 2019-2025 SUSE LLC
 #
-# This program is free software: you can redistribute it and/or modify it under
-# the terms of the GNU Lesser General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or (at your option) any
-# later version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-# PARTICULAR PURPOSE. See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License along
-# with this program. If not, see <https://www.gnu.org/licenses/>.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 
 .PHONY: smoke-test
 smoke-test:

examples/c/Makefile
@@ -1,20 +1,13 @@
-# SPDX-License-Identifier: LGPL-3.0-or-later
+# SPDX-License-Identifier: MPL-2.0
 #
 # libpathrs: safe path resolution on Linux
 # Copyright (C) 2019-2025 Aleksa Sarai <cyphar@cyphar.com>
 # Copyright (C) 2019-2025 SUSE LLC
 #
-# This program is free software: you can redistribute it and/or modify it under
-# the terms of the GNU Lesser General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or (at your option) any
-# later version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-# PARTICULAR PURPOSE. See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License along
-# with this program. If not, see <https://www.gnu.org/licenses/>.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 
 CC ?= gcc
 

examples/c/cat.c
@@ -1,20 +1,12 @@
-// SPDX-License-Identifier: LGPL-3.0-or-later
+// SPDX-License-Identifier: MPL-2.0
 /*
  * libpathrs: safe path resolution on Linux
  * Copyright (C) 2019-2025 Aleksa Sarai <cyphar@cyphar.com>
  * Copyright (C) 2019-2025 SUSE LLC
  *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License along
- * with this program. If not, see <https://www.gnu.org/licenses/>.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
 /*

examples/c/cat_multithread.c
@@ -1,20 +1,12 @@
-// SPDX-License-Identifier: LGPL-3.0-or-later
+// SPDX-License-Identifier: MPL-2.0
 /*
  * libpathrs: safe path resolution on Linux
  * Copyright (C) 2019-2025 Aleksa Sarai <cyphar@cyphar.com>
  * Copyright (C) 2019-2025 SUSE LLC
  *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License along
- * with this program. If not, see <https://www.gnu.org/licenses/>.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
 /*

examples/go/Makefile
@@ -1,20 +1,12 @@
-# SPDX-License-Identifier: LGPL-3.0-or-later
+# SPDX-License-Identifier: MPL-2.0
 #
 # libpathrs: safe path resolution on Linux
 # Copyright (C) 2019-2025 Aleksa Sarai <cyphar@cyphar.com>
 # Copyright (C) 2019-2025 SUSE LLC
 #
-# This program is free software: you can redistribute it and/or modify it under
-# the terms of the GNU Lesser General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or (at your option) any
-# later version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-# PARTICULAR PURPOSE. See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License along
-# with this program. If not, see <https://www.gnu.org/licenses/>.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 GO ?= go
 

examples/go/cat.go
@@ -1,24 +1,16 @@
-// SPDX-License-Identifier: LGPL-3.0-or-later
+// SPDX-License-Identifier: MPL-2.0
 /*
  * libpathrs: safe path resolution on Linux
  * Copyright (C) 2019-2025 Aleksa Sarai <cyphar@cyphar.com>
  * Copyright (C) 2019-2025 SUSE LLC
  *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License along
- * with this program. If not, see <https://www.gnu.org/licenses/>.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
 // Original author of this example code:
-// Copyright (C) 2020 Maxim Zhiburt <zhiburt@gmail.com>
+// Maxim Zhiburt <zhiburt@gmail.com> (2020)
 
 // File: examples/c/cat.c
 //

examples/go/sysctl.go
@@ -1,20 +1,12 @@
-// SPDX-License-Identifier: LGPL-3.0-or-later
+// SPDX-License-Identifier: MPL-2.0
 /*
  * libpathrs: safe path resolution on Linux
  * Copyright (C) 2019-2025 Aleksa Sarai <cyphar@cyphar.com>
  * Copyright (C) 2019-2025 SUSE LLC
  *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License along
- * with this program. If not, see <https://www.gnu.org/licenses/>.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
 package main

commit 92fb1717b1fb86028189f52fd328fddfa911cc7b

examples: python: remove procfs example
This example was accidentally added in b2f24a201b8e ("examples:
relicense to MPL-2.0") and was actually only really intended as a
prototype for the e2e python tests...

Fixes: b2f24a201b8e ("examples: relicense to MPL-2.0")
Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

examples/python/procfs.py
@@ -1,167 +1,0 @@
-#!/usr/bin/env python3
-# libpathrs: safe path resolution on Linux
-# Copyright (C) 2019-2024 Aleksa Sarai <cyphar@cyphar.com>
-# Copyright (C) 2019-2024 SUSE LLC
-#
-# This program is free software: you can redistribute it and/or modify it under
-# the terms of the GNU Lesser General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or (at your option) any
-# later version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-# details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
-
-# File: examples/python/procfs.py
-#
-# An example program which does some basic procfs operations.
-
-import os
-import sys
-
-sys.path.append(os.path.dirname(__file__) + "/../contrib/bindings/python")
-from pathrs import procfs
-
-
-def chomp(s):
-    for nl in ["\r\n", "\r", "\n"]:
-        if s.endswith(nl):
-            return s[: -len(nl)]
-    return s
-
-
-# TODO: Pretty-print errors...
-
-
-def base_str(args):
-    base = args.procfs_base
-    if base == procfs.PROC_ROOT:
-        return "/proc"
-    elif base == procfs.PROC_SELF:
-        return "/proc/self"
-    elif base == procfs.PROC_THREAD_SELF:
-        return "/proc/thread-self"
-    elif args._pid is not None:
-        return f"/proc/{args._pid}"
-    return "<unknown proc base>"
-
-
-def do_open(args):
-    base = args.procfs_base
-    subpath = args.subpath
-
-    # TODO: Support O_* flags.
-    extra_flags = 0
-    if not args.follow_trailing:
-        extra_flags |= os.O_NOFOLLOW
-
-    if args.debug:
-        print(f"fullpath: {base_str(args)}/{subpath}")
-        print("---")
-
-    with procfs.open(base, subpath, extra_flags=extra_flags) as f:
-        for line in f:
-            print(chomp(line))
-
-
-def do_readlink(args):
-    base = args.procfs_base
-    subpath = args.subpath
-
-    if args.debug:
-        print(f"fullpath: {base_str(args)}/{subpath}")
-        print("---")
-
-    link_target = procfs.readlink(base, subpath)
-    print(link_target)
-
-
-def main(*args):
-    import argparse
-
-    class ProcPidAction(argparse.Action):
-        def __init__(self, option_strings, dest, nargs=None, **kwargs):
-            if nargs is not None:
-                raise ValueError("nargs not allowed")
-            super().__init__(option_strings, dest, **kwargs)
-
-        def __call__(self, parser, namespace, values, option_string=None):
-            pid = int(values)
-            setattr(namespace, "_pid", pid)
-            setattr(namespace, self.dest, procfs.PROC_PID(pid))
-
-    parser = argparse.ArgumentParser(prog="procfs.py")
-    parser.add_argument("--debug", action="store_true", help="output debug messages")
-
-    procfs_base = parser.add_argument_group(
-        title="base path",
-        description="Specify what base directory to use as the root of the procfs operation.",
-    ).add_mutually_exclusive_group(required=True)
-
-    procfs_base.add_argument(
-        "-R",
-        "--root",
-        dest="procfs_base",
-        action="store_const",
-        const=procfs.PROC_ROOT,
-        help="/proc",
-    )
-    procfs_base.add_argument(
-        "-s",
-        "--self",
-        dest="procfs_base",
-        action="store_const",
-        const=procfs.PROC_SELF,
-        help="/proc/self",
-    )
-    procfs_base.add_argument(
-        "--thread-self",
-        dest="procfs_base",
-        action="store_const",
-        const=procfs.PROC_THREAD_SELF,
-        help="/proc/thread-self",
-    )
-    procfs_base.add_argument(
-        "-p",
-        "--pid",
-        metavar="PID",
-        dest="procfs_base",
-        action=ProcPidAction,
-        help="/proc/$PID (racey)",
-    )
-    procfs_base.set_defaults(_pid=None)
-
-    subparser = parser.add_subparsers(help="procfs operations")
-
-    # procfs.py readlink <subpath>
-    readlink_cmd = subparser.add_parser("readlink", help="safe readlink")
-    readlink_cmd.set_defaults(func=do_readlink)
-
-    # procfs.py open <subpath>
-    open_cmd = subparser.add_parser("open", help="open(O_NOFOLLOW)")
-    open_cmd.add_argument(
-        "--follow-trailing",
-        dest="follow_trailing",
-        action=argparse.BooleanOptionalAction,
-        default=True,
-        help="whether to follow trailing symlinks when opening paths (this allows you to re-open magic-links)",
-    )
-    # TODO: Support O_* flags.
-    open_cmd.set_defaults(func=do_open)
-
-    parser.add_argument("subpath")
-
-    args = parser.parse_args(args)
-    if args.debug:
-        print(f"procfs base: {args.procfs_base:#x} (pid={args._pid})")
-        print(f"subcommand: {args.func}")
-        print(f"subpath: {args.subpath}")
-    args.func(args)
-
-
-if __name__ == "__main__":
-    main(*sys.argv[1:])