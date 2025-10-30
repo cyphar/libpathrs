367b58fc49dc961731abc17929200d57a800eb04
Aleksa Sarai <cyphar@cyphar.com>
2025-10-30 22:51:12 +1100

    contrib: add fake-enosys helper
    The idea is quite simple -- generate a very minimal seccomp-bpf profile
    which returns ENOSYS if the syscall matches the syscalls specified and
    then execve(2) the provided command.
    
    Luckily the (apparently unmaintained) bpfvm crate provides enough rope
    to hang ourselves without needing to resort to libseccomp. We also don't
    really need to deal with multiarch because we are only ever dealing with
    the native architecture we run the tests on, so we can just use the raw
    syscall numbers for our local architecture.
    
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

---
+++ contrib/fake-enosys/src/bpf.rs
@@ -0,0 +1,155 @@
+// SPDX-License-Identifier: MPL-2.0
+/*
+ * libpathrs: safe path resolution on Linux
+ * Copyright (C) 2019-2025 Aleksa Sarai <cyphar@cyphar.com>
+ * Copyright (C) 2019-2025 SUSE LLC
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+use anyhow::{Context, Error};
+use syscalls::Sysno;
+
+pub(crate) fn compile_filter(
+    syscalls: impl AsRef<[Sysno]>,
+) -> Result<Vec<libc::sock_filter>, Error> {
+    use bpfvm::{
+        asm::{self, Operation::*},
+        bpf::{JmpOp::*, Mode::*, Src::*},
+        seccomp::{FieldOffset, SeccompReturn},
+    };
+
+    // Generate a very basic seccomp-bpf profile:
+    asm::compile(
+        &vec![
+            // TODO: Check that the architecture is native...
+            // load [0] (syscall number)
+            Load(ABS, FieldOffset::Syscall.offset()),
+        ]
+        .into_iter()
+        // jeq [$sysno1],[ENOSYS]
+        // jeq [$sysno2],[ENOSYS]
+        // ...
+        .chain(
+            syscalls
+                .as_ref()
+                .iter()
+                .flat_map(|sysno| Some(Jump(JEQ, sysno.id() as u32, Some("ENOSYS"), None))),
+        )
+        // ret [0]
+        // 'ENOSYS:
+        // ret [ENOSYS]
+        .chain(vec![
+            Label("ALLOW"),
+            Return(Const, SeccompReturn::Allow.into()),
+            Label("ENOSYS"),
+            Return(Const, SeccompReturn::Errno(libc::ENOSYS as u32).into()),
+        ])
+        .collect::<Vec<_>>(),
+    )
+    .context("failed to compile seccomp-bpf filter")
+}
+
+#[cfg(test)]
+#[cfg(target_arch = "x86_64")] // for SECCOMP_ARCH_NATIVE
+mod test {
+    use super::*;
+
+    use anyhow::Error;
+    use bpfvm::{
+        seccomp::SeccompReturn,
+        vm::{self, BpfVM},
+    };
+    use pretty_assertions::assert_eq;
+    use syscalls::Sysno;
+
+    fn syscall_data(sysno: Sysno) -> libc::seccomp_data {
+        libc::seccomp_data {
+            nr: sysno.id(),
+            arch: SECCOMP_ARCH_NATIVE,
+            instruction_pointer: 0xdeadbeefcafe,
+            args: [0x0, 0x0, 0x0, 0x0, 0x0, 0x0],
+        }
+    }
+
+    #[cfg(target_arch = "x86_64")]
+    const SECCOMP_ARCH_NATIVE: u32 = bpfvm::seccomp::AUDIT_ARCH_X86_64;
+
+    #[test]
+    fn test_single_filter_allow() -> Result<(), Error> {
+        let filter = compile_filter(&[Sysno::openat])?;
+
+        let data = syscall_data(Sysno::link);
+        let ret: SeccompReturn = BpfVM::new(&filter)?
+            .run(&vm::any_to_data(&data))?
+            .try_into()?;
+        assert_eq!(ret, SeccompReturn::Allow, "seccomp should allow link");
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_multi_filter_allow() -> Result<(), Error> {
+        let filter = compile_filter(&[Sysno::openat2, Sysno::statx])?;
+
+        let data = syscall_data(Sysno::openat);
+        let ret: SeccompReturn = BpfVM::new(&filter)?
+            .run(&vm::any_to_data(&data))?
+            .try_into()?;
+        assert_eq!(ret, SeccompReturn::Allow, "seccomp should allow openat");
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_single_filter_enosys() -> Result<(), Error> {
+        let filter = compile_filter(&[Sysno::openat2])?;
+
+        let data = syscall_data(Sysno::openat2);
+        let ret: SeccompReturn = BpfVM::new(&filter)?
+            .run(&vm::any_to_data(&data))?
+            .try_into()?;
+        assert_eq!(
+            ret,
+            SeccompReturn::Errno(libc::ENOSYS as u32),
+            "errno should be ENOSYS for statx"
+        );
+
+        let data = syscall_data(Sysno::openat);
+        let ret: SeccompReturn = BpfVM::new(&filter)?
+            .run(&vm::any_to_data(&data))?
+            .try_into()?;
+        assert_eq!(ret, SeccompReturn::Allow, "seccomp should allow openat");
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_multi_filter_enosys() -> Result<(), Error> {
+        let filter = compile_filter(&[Sysno::openat2, Sysno::statx])?;
+
+        let data = syscall_data(Sysno::statx);
+        let ret: SeccompReturn = BpfVM::new(&filter)?
+            .run(&vm::any_to_data(&data))?
+            .try_into()?;
+        assert_eq!(
+            ret,
+            SeccompReturn::Errno(libc::ENOSYS as u32),
+            "errno should be ENOSYS for statx"
+        );
+
+        let data = syscall_data(Sysno::openat2);
+        let ret: SeccompReturn = BpfVM::new(&filter)?
+            .run(&vm::any_to_data(&data))?
+            .try_into()?;
+        assert_eq!(
+            ret,
+            SeccompReturn::Errno(libc::ENOSYS as u32),
+            "errno should be ENOSYS for statx"
+        );
+
+        Ok(())
+    }
+}
---
+++ contrib/fake-enosys/src/main.rs
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: MPL-2.0
+/*
+ * libpathrs: safe path resolution on Linux
+ * Copyright (C) 2019-2025 Aleksa Sarai <cyphar@cyphar.com>
+ * Copyright (C) 2019-2025 SUSE LLC
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+mod bpf;
+
+use std::{os::unix::process::CommandExt, process::Command as StdCmd};
+
+use anyhow::{anyhow, Context, Error};
+use clap::{Arg, ArgAction, Command};
+use rustix::{process as rustix_process, thread as rustix_thread};
+use syscalls::Sysno;
+
+fn cli() -> Command {
+    Command::new("fake-enosys")
+        .author("Aleksa Sarai <cyphar@cyphar.com>")
+        .about("Runs a subcommand with certain syscalls disabled (returning -ENOSYS).")
+        .arg(
+            Arg::new("syscalls")
+                .long("syscall")
+                .short('s')
+                .help("syscall name (or number) to mask (comma-separated or passed multiple times)")
+                .action(ArgAction::Append),
+        )
+        .arg(
+            Arg::new("command")
+                .help("the subcommand to run")
+                .allow_hyphen_values(true)
+                .num_args(..)
+                .trailing_var_arg(true)
+                .required(true),
+        )
+}
+
+#[test]
+fn verify_app() {
+    cli().debug_assert();
+}
+
+fn seccomp_set_filter(mut filter: impl AsMut<[libc::sock_filter]>) -> Result<(), Error> {
+    let filter = filter.as_mut();
+
+    let bpf_prog = libc::sock_fprog {
+        len: filter.len() as u16,
+        filter: filter.as_mut_ptr(),
+    };
+
+    // SAFETY: Safe libc function and the kernel copies the filter internally so
+    // the lifetime is not an issue.
+    let ret = unsafe {
+        libc::prctl(
+            libc::PR_SET_SECCOMP,
+            libc::SECCOMP_MODE_FILTER,
+            &bpf_prog as *const _,
+        )
+    };
+    if ret < 0 {
+        Err(std::io::Error::last_os_error()).context("failed to install seccomp filter")?;
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<(), Error> {
+    let m = cli().get_matches();
+
+    let syscalls: Vec<Sysno> = m
+        .get_many::<String>("syscalls")
+        .map(|iter| {
+            iter.flat_map(|s| s.split(","))
+                .filter(|&s| s != "")
+                .map(|syscall| -> Result<_, Error> {
+                    syscall.parse::<Sysno>().or_else(|_| {
+                        syscall
+                            .parse::<usize>()
+                            .map_err(|_| anyhow!("syscall {syscall:?} is not a known syscall"))
+                            .and_then(|sysno| {
+                                Sysno::new(sysno).ok_or_else(|| {
+                                    anyhow!("syscall #{sysno} is not a known syscall")
+                                })
+                            })
+                    })
+                })
+                .collect::<Result<Vec<_>, _>>()
+        })
+        .unwrap_or_else(|| Ok(vec![]))?;
+
+    let cmdline: Vec<_> = m
+        .get_many::<String>("command")
+        .ok_or_else(|| anyhow!("no command found"))?
+        .cloned()
+        .collect();
+    let (prog, args) = cmdline
+        .split_first()
+        .context("command-line must have at least one element")?;
+
+    if !syscalls.is_empty() {
+        let mut filter = bpf::compile_filter(&syscalls)?;
+
+        // Unprivileged processes cannot enable seccomp-bpf unless they also set the
+        // no-new-privs bit (to stop them from being able to trick setuid binaries).
+        if !rustix_process::getuid().is_root() {
+            rustix_thread::set_no_new_privs(true).context("could not set no-new-privs bit")?;
+        }
+
+        seccomp_set_filter(&mut filter)?;
+    }
+
+    Err(StdCmd::new(prog).args(args).exec()).context("could not exec subcommand")
+}
