cyphar/libpathrs

commit 3fe557ea9cd80169725a7a7467cf51380810cce4

build: fix Rust 1.63 builds broken by --crate-type
cargo build --crate-type= was stabilised in Rust 1.64, so we need to use
the old method of enabling cdylib and staticlib builds for all builds
(regardless of the capi feature).

This was added by 493cdb2f8e13 ("capi: make opt-in via 'capi' feature"),
but the lack of a CI hook checking that the build worked meaned that we
missed that this was actually broken on Rust 1.63.

Debian bookworm still has Rust 1.63 and will be maintained until June
2026 (with LTS lasting until 2028). As such, we should keep our new
releases supporting 1.63 until June 2026 just in case someone decides to
package libpathrs for Debian oldstable.

Fixes: 493cdb2f8e13 ("capi: make opt-in via 'capi' feature")
Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

Makefile
@@ -29,6 +29,8 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+SHELL := /bin/bash
+
 CARGO ?= cargo
 CARGO_NIGHTLY ?= cargo +nightly
 
@@ -48,8 +50,8 @@ CARGO_CHECK := $(call cargo_hack,$(CARGO),check)
 CARGO_CLIPPY := $(call cargo_hack,$(CARGO),clippy)
 CARGO_LLVM_COV := $(call cargo_hack,$(CARGO_NIGHTLY),llvm-cov)
 
-RUSTC_FLAGS := --features=capi -- -C panic=abort
-CARGO_FLAGS ?=
+RUSTC_FLAGS := -C panic=abort
+CARGO_FLAGS ?= --features=capi
 
 SRC_FILES = $(wildcard Cargo.*) $(shell find . -name '*.rs')
 
@@ -58,21 +60,30 @@ SRC_FILES = $(wildcard Cargo.*) $(shell find . -name '*.rs')
 debug: target/debug
 
 target/debug: $(SRC_FILES)
+	# MSRV(1.64): Use cargo rustc --crate-type={cdy,static}lib.
+	sed -i.orig '/^crate-type/ s/=.*/= ["rlib", "cdylib", "staticlib"]/' Cargo.toml
+	RUSTFLAGS="$(RUSTC_FLAGS)" $(CARGO) build $(CARGO_FLAGS)
+	mv Cargo.toml{.orig,}
 	# For some reason, --crate-types needs separate invocations. We can't use
 	# #![crate_type] unfortunately, as using it with #![cfg_attr] has been
 	# deprecated. <https://github.com/rust-lang/rust/issues/91632>
-	$(CARGO) rustc $(CARGO_FLAGS) --crate-type=cdylib    $(RUSTC_FLAGS)
-	$(CARGO) rustc $(CARGO_FLAGS) --crate-type=staticlib $(RUSTC_FLAGS)
+	#$(CARGO) rustc $(CARGO_FLAGS) --crate-type=cdylib    $(RUSTC_FLAGS)
+	#$(CARGO) rustc $(CARGO_FLAGS) --crate-type=staticlib $(RUSTC_FLAGS)
 
 .PHONY: release
 release: target/release
 
+target/release: CARGO_FLAGS += --release
 target/release: $(SRC_FILES)
+	# MSRV(1.64): Use cargo rustc --crate-type={cdy,static}lib.
+	sed -i.orig '/^crate-type/ s/=.*/= ["rlib", "cdylib", "staticlib"]/' Cargo.toml
+	RUSTFLAGS="$(RUSTC_FLAGS)" $(CARGO) build $(CARGO_FLAGS)
+	mv Cargo.toml{.orig,}
 	# For some reason, --crate-types needs separate invocations. We can't use
 	# #![crate_type] unfortunately, as using it with #![cfg_attr] has been
 	# deprecated. <https://github.com/rust-lang/rust/issues/91632>
-	$(CARGO) rustc $(CARGO_FLAGS) --release --crate-type=cdylib    $(RUSTC_FLAGS)
-	$(CARGO) rustc $(CARGO_FLAGS) --release --crate-type=staticlib $(RUSTC_FLAGS)
+	#$(CARGO) rustc $(CARGO_FLAGS) --crate-type=cdylib    $(RUSTC_FLAGS)
+	#$(CARGO) rustc $(CARGO_FLAGS) --crate-type=staticlib $(RUSTC_FLAGS)
 
 .PHONY: smoke-test
 smoke-test:

Cargo.toml
@@ -48,7 +48,7 @@ rust-version = "1.63"
 maintenance = { status = "experimental" }
 
 [lib]
-# When building the CAPI, our Makefile adds --crate-type={cdylib,staticlib}.
+# MSRV(1.64): Replace with --crate-type={cdy,static}lib in Makefile.
 crate-type = ["rlib"]
 
 [features]

commit 90239f68aadf4b30ff63addb2facc75ee0727ac9

make: improve --crate-types workaround
If the build fails we end up with a modified Cargo.toml, which would be
very easy to accidentally commit. So use a wrapper script to ensure the
cleanup is done properly (with "trap ... EXIT").

Fixes: 3fe557ea9cd8 ("build: fix Rust 1.63 builds broken by --crate-type")
Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

Makefile
@@ -29,8 +29,6 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-SHELL := /bin/bash
-
 CARGO ?= cargo
 CARGO_NIGHTLY ?= cargo +nightly
 
@@ -61,10 +59,8 @@ debug: target/debug
 
 target/debug: $(SRC_FILES)
 	# MSRV(1.64): Use cargo rustc --crate-type={cdy,static}lib.
-	sed -i.orig '/^crate-type/ s/=.*/= ["rlib", "cdylib", "staticlib"]/' Cargo.toml
-	RUSTFLAGS="$(RUSTC_FLAGS)" $(CARGO) build $(CARGO_FLAGS)
-	mv Cargo.toml{.orig,}
-	# For some reason, --crate-types needs separate invocations. We can't use
+	RUSTFLAGS="$(RUSTC_FLAGS)" ./hack/with-crate-type.sh $(CARGO) build $(CARGO_FLAGS)
+	# For some reason, --crate-type needs separate invocations. We can't use
 	# #![crate_type] unfortunately, as using it with #![cfg_attr] has been
 	# deprecated. <https://github.com/rust-lang/rust/issues/91632>
 	#$(CARGO) rustc $(CARGO_FLAGS) --crate-type=cdylib    $(RUSTC_FLAGS)
@@ -76,10 +72,8 @@ release: target/release
 target/release: CARGO_FLAGS += --release
 target/release: $(SRC_FILES)
 	# MSRV(1.64): Use cargo rustc --crate-type={cdy,static}lib.
-	sed -i.orig '/^crate-type/ s/=.*/= ["rlib", "cdylib", "staticlib"]/' Cargo.toml
-	RUSTFLAGS="$(RUSTC_FLAGS)" $(CARGO) build $(CARGO_FLAGS)
-	mv Cargo.toml{.orig,}
-	# For some reason, --crate-types needs separate invocations. We can't use
+	RUSTFLAGS="$(RUSTC_FLAGS)" ./hack/with-crate-type.sh $(CARGO) build $(CARGO_FLAGS)
+	# For some reason, --crate-type needs separate invocations. We can't use
 	# #![crate_type] unfortunately, as using it with #![cfg_attr] has been
 	# deprecated. <https://github.com/rust-lang/rust/issues/91632>
 	#$(CARGO) rustc $(CARGO_FLAGS) --crate-type=cdylib    $(RUSTC_FLAGS)

hack/with-crate-type.sh
@@ -1,0 +1,47 @@
+#!/bin/bash
+# SPDX-License-Identifier: MPL-2.0 OR LGPL-3.0-or-later
+#
+# libpathrs: safe path resolution on Linux
+# Copyright (C) 2026 Aleksa Sarai <cyphar@cyphar.com>
+#
+# == MPL-2.0 ==
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# Alternatively, this Source Code Form may also (at your option) be used
+# under the terms of the GNU Lesser General Public License Version 3, as
+# described below:
+#
+# == LGPL-3.0-or-later ==
+#
+#  This program is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or (at
+#  your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+#  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+#  for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+# cargo's --crate-type= argument was stabilised in Rust 1.64 but our MSRV is
+# 1.63. This script emulates --crate-type= by temporarily modifying the
+# crate-type option in Cargo.toml.
+
+set -Eeuo pipefail
+
+SRC_ROOT="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/..")"
+backup="$(mktemp "$SRC_ROOT/Cargo.toml.XXXXXX")"
+
+set -x
+
+sed -i".${backup##*.}" '/^crate-type/ s/=.*/= ["rlib", "cdylib", "staticlib"]/' "$SRC_ROOT/Cargo.toml"
+# shellcheck disable=SC2064 # We want to expand the variables immediately.
+trap "mv '$backup' '$SRC_ROOT/Cargo.toml'" EXIT
+
+"$@"