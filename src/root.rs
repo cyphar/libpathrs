cyphar/libpathrs

commit 775be64a3c9bde22dbc418f3733d26b323684ba0

Root::create_file: take open flags to configure returned fd
Some users might not want O_EXCL, and since we aren't going to return an
O_PATH descriptor we might as well let them configure everything.

Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

src/root.rs
@@ -23,7 +23,7 @@ use crate::{
     resolvers::Resolver,
     syscalls,
     utils::RawFdExt,
-    Handle,
+    Handle, OpenFlags,
 };
 
 use std::{
@@ -371,6 +371,7 @@ impl Root {
     pub fn create_file<P: AsRef<Path>>(
         &self,
         path: P,
+        flags: OpenFlags,
         perm: &Permissions,
     ) -> Result<Handle, Error> {
         // Get a handle for the lexical parent of the target path. It must
@@ -385,14 +386,14 @@ impl Root {
         let dirfd = dir.as_raw_fd();
 
         // XXX: openat2(2) supports doing O_CREAT on trailing symlinks without
-        //      O_NOFOLLOW. We might want to expose that here, though because it
-        //      can't be done with the emulated backend that might be a bad
-        //      idea.
-        let file = syscalls::openat(dirfd, name, libc::O_CREAT | libc::O_EXCL, perm.mode())
-            .context(error::RawOsError {
+        // O_NOFOLLOW. We might want to expose that here, though because it
+        // can't be done with the emulated backend that might be a bad idea.
+        let flags = flags.0 | libc::O_CREAT;
+        let file =
+            syscalls::openat(dirfd, name, flags, perm.mode()).context(error::RawOsError {
                 operation: "pathrs create_file",
             })?;
-        // TODO: We should probably turn this to an `O_PATH`...
+
         Ok(Handle::from_file_unchecked(file))
     }
 

include/pathrs.h
@@ -197,11 +197,17 @@ int pathrs_rename(int root_fd,
 
 /**
  * Create a new regular file within the rootfs referenced by root_fd. This is
- * effectively an O_CREAT|O_EXCL operation, and so (unlike pathrs_resolve()),
- * this function can be used on non-existent paths.
+ * effectively an O_CREAT operation, and so (unlike pathrs_resolve()), this
+ * function can be used on non-existent paths.
+ *
+ * If you want to ensure the creation is a new file, use O_EXCL.
  *
  * If you want to create a file without opening a handle to it, you can do
  * pathrs_mknod(root_fd, path, S_IFREG|mode, 0) instead.
+ *
+ * As with pathrs_reopen(), O_NOCTTY is automatically set when opening the
+ * path. If you want to use the path as a controlling terminal, you will have
+ * to do ioctl(fd, TIOCSCTTY, 0) yourself.
  *
  * NOTE: Unlike O_CREAT, pathrs_creat() will return an error if the final
  * component is a dangling symlink. O_CREAT will create such files, and while
@@ -211,13 +217,14 @@ int pathrs_rename(int root_fd,
  * # Return Value
  *
  * On success, this function returns a file descriptor to the requested file.
+ * The open flags are based on the provided flags.
  *
  * If an error occurs, this function will return a negative error code. To
  * retrieve information about the error (such as a string describing the error,
  * the system errno(7) value associated with the error, etc), use
  * pathrs_errorinfo().
  */
-int pathrs_creat(int root_fd, const char *path, unsigned int mode);
+int pathrs_creat(int root_fd, const char *path, int flags, unsigned int mode);
 
 /**
  * Create a new directory within the rootfs referenced by root_fd.

src/capi/core.rs
@@ -162,11 +162,17 @@ pub extern "C" fn pathrs_rename(
 // TODO: Replace all these wrappers with macros. It's quite repetitive.
 
 /// Create a new regular file within the rootfs referenced by root_fd. This is
-/// effectively an O_CREAT|O_EXCL operation, and so (unlike pathrs_resolve()),
-/// this function can be used on non-existent paths.
+/// effectively an O_CREAT operation, and so (unlike pathrs_resolve()), this
+/// function can be used on non-existent paths.
+///
+/// If you want to ensure the creation is a new file, use O_EXCL.
 ///
 /// If you want to create a file without opening a handle to it, you can do
 /// pathrs_mknod(root_fd, path, S_IFREG|mode, 0) instead.
+///
+/// As with pathrs_reopen(), O_NOCTTY is automatically set when opening the
+/// path. If you want to use the path as a controlling terminal, you will have
+/// to do ioctl(fd, TIOCSCTTY, 0) yourself.
 ///
 /// NOTE: Unlike O_CREAT, pathrs_creat() will return an error if the final
 /// component is a dangling symlink. O_CREAT will create such files, and while
@@ -176,17 +182,23 @@ pub extern "C" fn pathrs_rename(
 /// # Return Value
 ///
 /// On success, this function returns a file descriptor to the requested file.
+/// The open flags are based on the provided flags.
 ///
 /// If an error occurs, this function will return a negative error code. To
 /// retrieve information about the error (such as a string describing the error,
 /// the system errno(7) value associated with the error, etc), use
 /// pathrs_errorinfo().
 #[no_mangle]
-pub extern "C" fn pathrs_creat(root_fd: RawFd, path: *const c_char, mode: c_uint) -> RawFd {
+pub extern "C" fn pathrs_creat(
+    root_fd: RawFd,
+    path: *const c_char,
+    flags: c_int,
+    mode: c_uint,
+) -> RawFd {
     ret::with_fd(root_fd, |root: &mut Root| {
         let mode = mode & !libc::S_IFMT;
         let perm = Permissions::from_mode(mode);
-        root.create_file(parse_path(path)?, &perm)
+        root.create_file(parse_path(path)?, OpenFlags(flags), &perm)
     })
 }
 

commit b65afadbad052ad230387c943842fae512c5ab13

root: improve Root::create{,_file} docs
The previous docs incorrectly implied that Root::create_file would
return EEXIST if the file already exists -- this was the case until
commit 775be64a3c9b ("Root::create_file: take open flags to configure
returned fd") but the docs were not updated.

Fixes: 775be64a3c9b ("Root::create_file: take open flags to configure returned fd")
Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

src/root.rs
@@ -361,8 +361,11 @@ impl Root {
     ///
     /// # Errors
     ///
-    /// If the path already exists (regardless of the type of the existing
-    /// inode), an error is returned.
+    /// In addition to the regular filesystem errors that can be returned, note
+    /// that if the path already exists (regardless of the type of the existing
+    /// inode), [`EEXIST`] will be returned.
+    ///
+    /// [`EEXIST`]: std::io::ErrorKind::AlreadyExists
     #[doc(alias = "pathrs_inroot_mkdir")]
     #[doc(alias = "pathrs_inroot_mknod")]
     #[doc(alias = "pathrs_inroot_symlink")]
@@ -391,7 +394,9 @@ impl Root {
     ///
     /// # Errors
     ///
-    /// Identical to [`create`].
+    /// Mostly identical to [`create`], except that an error will only be
+    /// returned if the path already exists if and only if [`OpenFlags::O_EXCL`]
+    /// is set in `flags`.
     ///
     /// [`create`]: Self::create
     /// [`create_file`]: Self::create_file
@@ -879,8 +884,11 @@ impl RootRef<'_> {
     ///
     /// # Errors
     ///
-    /// If the path already exists (regardless of the type of the existing
-    /// inode), an error is returned.
+    /// In addition to the regular filesystem errors that can be returned, note
+    /// that if the path already exists (regardless of the type of the existing
+    /// inode), [`EEXIST`] will be returned.
+    ///
+    /// [`EEXIST`]: std::io::ErrorKind::AlreadyExists
     #[doc(alias = "pathrs_inroot_mkdir")]
     #[doc(alias = "pathrs_inroot_mknod")]
     #[doc(alias = "pathrs_inroot_symlink")]
@@ -979,7 +987,9 @@ impl RootRef<'_> {
     ///
     /// # Errors
     ///
-    /// Identical to [`create`].
+    /// Mostly identical to [`create`], except that an error will only be
+    /// returned if the path already exists if and only if [`OpenFlags::O_EXCL`]
+    /// is set in `flags`.
     ///
     /// [`create`]: Self::create
     /// [`create_file`]: Self::create_file