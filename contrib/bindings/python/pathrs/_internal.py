cyphar/libpathrs

commit 797b29eb696b48050773d5b4101759527ca0f1c6

mypy: fix new errors
Signed-off-by: Aleksa Sarai <aleksa@amutable.com>

contrib/bindings/python/pathrs/_internal.py
@@ -93,7 +93,7 @@ class PathrsError(Exception):
             return None
 
         err = libpathrs_so.pathrs_errorinfo(err_id)
-        if err == ffi.NULL:  # type: ignore # TODO: Make this check nicer...
+        if err == ffi.NULL:  # TODO: Make this check nicer...
             return None
 
         description = _pystr(err.description)

e2e-tests/cmd/python/pathrs-cmd.py
@@ -571,6 +571,9 @@ if __name__ == "__main__":
     try:
         main(*sys.argv[1:])
     except pathrs.PathrsError as e:
-        print(f"ERRNO {e.errno} ({os.strerror(e.errno)})")
+        errstr = "<unknown error>"
+        if e.errno is not None:
+            errstr = os.strerror(e.errno)
+        print(f"ERRNO {e.errno} ({errstr})")
         print(f"error: {e.message}")
         sys.exit(1)

commit 1b38865c69953ae99f9fc919a8fef8278a4ea264

mypy: re-add ignore for ffi.NULL check
This is a partial revert of 797b29eb696b48050773d5b4101759527ca0f1c6.

For some reason CI doesn't like this, but my local mypy really needs
this ignore line, otherwise you get annoying errors like:

  $ make -C ./contrib/bindings/python lint
  pathrs/_internal.py:96: error: Non-overlapping equality check (left operand type: "CError", right operand type: "CType")  [comparison-overlap]

Fixes: 797b29eb696b ("mypy: fix new errors")
Signed-off-by: Aleksa Sarai <aleksa@amutable.com>

contrib/bindings/python/pathrs/_internal.py
@@ -93,7 +93,7 @@ class PathrsError(Exception):
             return None
 
         err = libpathrs_so.pathrs_errorinfo(err_id)
-        if err == ffi.NULL:  # TODO: Make this check nicer...
+        if err == ffi.NULL:  # type: ignore[comparison-overlap,unused-ignore] # TODO: Make this check nicer...
             return None
 
         description = _pystr(err.description)

contrib/bindings/python/setup.py
@@ -22,7 +22,7 @@ def parse_pyproject() -> Dict[str, Any]:
         openmode = "rb"
     except ImportError:
         # TODO: Remove this once we only support Python >= 3.11.
-        import toml as tomllib  # type: ignore
+        import toml as tomllib  # type: ignore # The types are not compatible.
 
         openmode = "r"
 