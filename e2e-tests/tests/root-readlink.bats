cyphar/libpathrs

commit e5d212dad1a4d1046138a4e5b2abbd93afb7fa4c

root: add readlink method
This is a minimal wrapper that users could probably DIY, but
readlink(fd, "") is a little subtle so it's better to provide a helper
than not.

Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

src/root.rs
@@ -265,7 +265,26 @@ impl Root {
         self.resolver.resolve(&self.inner, path, true)
     }
 
-    // TODO: readlink (need to move ResolverFlags out of Resolver)
+    /// Get the target of a symlink within a [`Root`].
+    ///
+    /// **NOTE**: The returned path is not modified to be "safe" outside of the
+    /// root. You should not use this path for doing further path lookups -- use
+    /// [`Root::resolve`] instead.
+    ///
+    /// This method is just shorthand for calling `readlinkat(2)` on the handle
+    /// returned by [`Root::resolve_nofollow`].
+    ///
+    /// [`Root`]: struct.Root.html
+    /// [`Root::resolve`]: struct.Root.html#method.resolve
+    /// [`Root::resolve_nofollow`]: struct.Root.html#method.resolve_nofollow
+    pub fn readlink<P: AsRef<Path>>(&self, path: P) -> Result<PathBuf, Error> {
+        let link = self
+            .resolve_nofollow(path)
+            .wrap("resolve symlink O_NOFOLLOW for readlink")?;
+        syscalls::readlinkat(link.as_file().as_raw_fd(), "").context(error::RawOsSnafu {
+            operation: "readlink resolve symlink",
+        })
+    }
 
     /// Within the [`Root`]'s tree, create an inode at `path` as specified by
     /// `inode_type`.

src/tests/test_root_ops.rs
@@ -307,11 +307,18 @@ mod utils {
                     }
                     // Check symlink is correct.
                     InodeType::Symlink(target) => {
+                        // Check using the a resolved handle.
                         let actual_target =
                             syscalls::readlinkat(created.as_file().as_raw_fd(), "")?;
                         assert_eq!(
                             target, actual_target,
                             "readlinkat(handle) link target mismatch"
+                        );
+                        // Double-check with Root::readlink.
+                        let actual_target = root.readlink(path)?;
+                        assert_eq!(
+                            target, actual_target,
+                            "root.readlink(path) link target mismatch"
                         );
                     }
                 }

commit 08856cca4c8267e4860e307cac572c81c5e0f0fd

root: make readlink return -EINVAL for non-symlinks
readlink() will return -EINVAL for non-symlinks, but the race-free
readlinkat(fd, "") approach returns -ENOENT if fd is not a symlink. This
mismatch can very easily cause confusion (in fact, I found this bug
while trying to use Root::readlink elsewhere).

As -ENOENT cannot be returned from readlinkat(fd, "") for any other
reason, map it to -EINVAL to match the API to what users would expect.
It's a bit strange the kernel does this, but oh well...

Fixes: e5d212dad1a4 ("root: add readlink method")
Signed-off-by: Aleksa Sarai <aleksa@amutable.com>

src/root.rs
@@ -870,7 +870,16 @@ impl RootRef<'_> {
         let link = self
             .resolve_nofollow(path)
             .wrap("resolve symlink O_NOFOLLOW for readlink")?;
-        syscalls::readlinkat(link, "").map_err(|err| {
+        syscalls::readlinkat(link, "").map_err(|mut err| {
+            // readlinkat(fd, "") returns ENOENT if the file descriptor is not a
+            // symlink, which is in stark contrast to the EINVAL you would
+            // normally get from readlinkat(dfd, path). As we know the target
+            // file exists at this point (because resolve_nofollow succeeded),
+            // we can just map ENOENT to EINVAL to reduce user confusion.
+            *err.errno_mut() = match err.errno() {
+                Errno::NOENT => Errno::INVAL,
+                errno => errno,
+            };
             ErrorImpl::RawOsError {
                 operation: "readlink resolve symlink".into(),
                 source: err,

src/syscalls.rs
@@ -283,32 +283,41 @@ pub(crate) enum Error {
     },
 }
 
+macro_rules! match_source_errno {
+    ($self:expr) => {
+        match $self {
+            Error::InvalidFd { source, .. }
+            | Error::Accessat { source, .. }
+            | Error::Openat { source, .. }
+            | Error::Openat2 { source, .. }
+            | Error::Readlinkat { source, .. }
+            | Error::Mkdirat { source, .. }
+            | Error::Mknodat { source, .. }
+            | Error::Unlinkat { source, .. }
+            | Error::Linkat { source, .. }
+            | Error::Symlinkat { source, .. }
+            | Error::Renameat { source, .. }
+            | Error::Renameat2 { source, .. }
+            | Error::Fstatfs { source, .. }
+            | Error::Fstatat { source, .. }
+            | Error::Statx { source, .. }
+            | Error::Fsopen { source, .. }
+            | Error::FsconfigCreate { source, .. }
+            | Error::FsconfigSetString { source, .. }
+            | Error::Fsmount { source, .. }
+            | Error::OpenTree { source, .. }
+            | Error::PrctlGet { source, .. } => source,
+        }
+    };
+}
+
 impl Error {
     pub(crate) fn errno(&self) -> Errno {
-        // XXX: This should probably be a macro...
-        *match self {
-            Error::InvalidFd { source, .. } => source,
-            Error::Accessat { source, .. } => source,
-            Error::Openat { source, .. } => source,
-            Error::Openat2 { source, .. } => source,
-            Error::Readlinkat { source, .. } => source,
-            Error::Mkdirat { source, .. } => source,
-            Error::Mknodat { source, .. } => source,
-            Error::Unlinkat { source, .. } => source,
-            Error::Linkat { source, .. } => source,
-            Error::Symlinkat { source, .. } => source,
-            Error::Renameat { source, .. } => source,
-            Error::Renameat2 { source, .. } => source,
-            Error::Fstatfs { source, .. } => source,
-            Error::Fstatat { source, .. } => source,
-            Error::Statx { source, .. } => source,
-            Error::Fsopen { source, .. } => source,
-            Error::FsconfigCreate { source, .. } => source,
-            Error::FsconfigSetString { source, .. } => source,
-            Error::Fsmount { source, .. } => source,
-            Error::OpenTree { source, .. } => source,
-            Error::PrctlGet { source, .. } => source,
-        }
+        *match_source_errno!(self)
+    }
+
+    pub(crate) fn errno_mut(&mut self) -> &mut Errno {
+        match_source_errno!(self)
     }
 
     // TODO: Switch to returning &Errno.

e2e-tests/tests/root-readlink.bats
@@ -49,12 +49,22 @@ function teardown() {
 	grep -Fx 'LINK-TARGET ../../link-b' <<<"$output"
 }
 
-@test "root readlink [non-symlink]" {
+@test "root readlink [file einval]" {
 	ROOT="$(setup_tmpdir)"
 
 	echo "/some/random/path" >"$ROOT/file"
 
 	pathrs-cmd root --root "$ROOT" readlink file
-	check-errno ENOENT
+	check-errno EINVAL
+	[[ "$output" == *"error:"*"readlinkat"* ]] # Make sure the error is from readlinkat(2).
+}
+
+@test "root readlink [dir einval]" {
+	ROOT="$(setup_tmpdir)"
+
+	mkdir -p "$ROOT/dir"
+
+	pathrs-cmd root --root "$ROOT" readlink dir
+	check-errno EINVAL
 	[[ "$output" == *"error:"*"readlinkat"* ]] # Make sure the error is from readlinkat(2).
 }

src/tests/test_root_ops.rs
@@ -595,6 +595,8 @@ root_op_tests! {
     root_dotdot: open_subpath("..", O_RDONLY) => Ok(".");
 
     enoent: readlink("non-exist") => Err(ErrorKind::OsError(Some(libc::ENOENT)));
+    dir_einval: readlink("b/c") => Err(ErrorKind::OsError(Some(libc::EINVAL)));
+    file_einval: readlink("b/c/file") => Err(ErrorKind::OsError(Some(libc::EINVAL)));
     abslink: readlink("e") => Ok("/b/c/d/e");
     rellink: readlink("b-file") => Ok("b/c/file");
     root: readlink("root-link1") => Ok("/");